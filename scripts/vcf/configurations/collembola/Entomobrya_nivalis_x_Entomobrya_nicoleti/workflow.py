39a6695e6f03af3f27e28300bc15c3253a1402f1
jeppebayer <jeppebayer@gmail.com>
2024-04-26 16:04:54 +0200

    26/04-2024

--- scripts/vcf/configurations/collembola/Entomobrya_nivalis_x_Entomobrya_nicoleti/workflow.py
+++ scripts/vcf/configurations/collembola/Entomobrya_nivalis_x_Entomobrya_nicoleti/workflow.py
@@ -3,4 +3,4 @@ import sys, os
 sys.path.insert(0, os.path.realpath('../../../workflow_source/'))
 from workflow_source import *
 
-gwf = freebayes_vcf_workflow()
+gwf = freebayes_population_set_workflow()
